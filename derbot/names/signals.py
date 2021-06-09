63a7594677bb7cd83c74d4638674bc4ba5095e55
Brian Dunnette <dunn0172@umn.edu>
2021-06-09 15:23:22 -0500

    tidying

---
+++ derbot/names/signals.py
@@ -0,0 +1,18 @@
+import random
+
+import humanize
+from derbot.names.models import DerbyName
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+
+@receiver(pre_save, sender=DerbyName)
+def generate_number(sender, instance, **kwargs):
+    if instance.cleared and not instance.number:
+        jersey_number = str(random.uniform(1, 9999))[
+            random.randint(0, 3) : random.randint(5, 7)
+        ].strip(".")
+        to_humanize = bool(random.getrandbits(1))
+        if to_humanize == True:
+            jersey_number = humanize.fractional(jersey_number).replace("/", "⁄")
+        instance.number = jersey_number
